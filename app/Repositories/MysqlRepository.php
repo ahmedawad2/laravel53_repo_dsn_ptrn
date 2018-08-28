4ca6f29f0b7a1951a052f827b1cca9ab4a55dc93
ahmedawad3 <a.awad@grandmstg.com>
2018-08-28 16:03:31 +0200

    adding the mysql version of data to be used by the users controller logic

---
+++ app/Repositories/MysqlRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Repositories;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MysqlRepository implements RepositoryInterface
+{
+    protected $model;
+
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
+    public function all()
+    {
+        return ($this->model)::all();
+    }
+
+    public function find($id)
+    {
+        return ($this->model)::find($id);
+    }
+}
