130ed1115fd39dc44a3397b547471df384c71f06
ahmedawad3 <a.awad@grandmstg.com>
2018-09-10 12:12:50 +0200

    commenting the define statements in MyConfigs.example.php, they were overriding the original values :D

--- config/MyConfigs.example.php
+++ config/MyConfigs.example.php
@@ -1,33 +1,33 @@
 <?php
 
 if (!defined('MONGO_HOST')) {
-    define('MONGO_HOST', '');
+//    define('MONGO_HOST', '');
 }
 if (!defined('MYSQL_HOST')) {
-    define('MYSQL_HOST', '');
+//    define('MYSQL_HOST', '');
 }
 
 if (!defined('MONGO_PORT')) {
-    define('MONGO_PORT', '');
+//    define('MONGO_PORT', '');
 }
 if (!defined('MYSQL_PORT')) {
-    define('MYSQL_PORT', '');
+//    define('MYSQL_PORT', '');
 }
 if (!defined('MYSQL_DB')) {
-    define('MYSQL_DB', '');
+//    define('MYSQL_DB', '');
 }
 if (!defined('MONGO_DB')) {
-    define('MONGO_DB', '');
+//    define('MONGO_DB', '');
 }
 if (!defined('MYSQL_USER')) {
-    define('MYSQL_USER', '');
+//    define('MYSQL_USER', '');
 }
 if (!defined('MYSQL_PASS')) {
-    define('MYSQL_PASS', '');
+//    define('MYSQL_PASS', '');
 }
 if (!defined('MONGO_USER')) {
-    define('MONGO_USER', '');
+//    define('MONGO_USER', '');
 }
 if (!defined('MONGO_PASS')) {
-    define('MONGO_PASS', '');
+//    define('MONGO_PASS', '');
 }
